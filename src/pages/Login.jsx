d75d18bb18656f8f8a66c0f4024d2c99aeb17415
rezanasu <rezanasution82@gmail.com>
2022-02-16 22:51:17 +0700

    init login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,43 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { 
+    ButtonGroup,
+    Button,
+    Grid,
+    Typography,
+    TextField
+} from '@mui/material';
 
 function Login() {
     return(
         <div className="Login">
-            <h1>LOGIN</h1>
+            <Grid container spacing={3} justifyContent="flex-end">
+                <Grid item>
+                    <Button color="primary" variant="contained" size="large">Sign In</Button>
+                </Grid>
+                <Grid item>
+                    <Button color="primary" variant="contained" size="large">Register</Button>
+                </Grid>
+            </Grid>
+            
+            <Grid container spacing={3} direction="row">
+                <Grid container item direction="column" spacing={2}>
+                    <Grid item>
+                        <Typography variant="h3">LOGIN</Typography>
+                    </Grid>
+                    <Grid item>
+                        <TextField variant="outlined" type="email" label="Email"/>
+                    </Grid>
+                    <Grid item>
+                        <TextField variant="outlined" type="password" label="Password"/>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="primary">Sign In</Button>
+                    </Grid>
+                </Grid>
+                <Grid container item>
+                    <Typography variant="h2">Images</Typography>
+                </Grid>
+            </Grid>
         </div>
     );
 }
